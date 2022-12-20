d6492e5a600f2eb1ec226743545b4af11eff0f13
Danilo Gonzalez Ardon <danilo.j.gonzalez@gmail.com>
2022-12-20 16:02:50 -0600

    bootstrap node.sh

---
+++ node.sh
@@ -0,0 +1,15 @@
+## Express TS
+# Initializae Express project
+npm init
+tsc --init
+
+npm install bcryptjs colors cookie-parser cors dotenv express express-mongo-sanitize express-rate-limit helmet hpp jsonwebtoken mongoose morgan nodemailer
+
+npm -D install typescript ts-node-dev @types/bcryptjs @types/colors @types/cookie-parser @types/cors @types/dotenv @types/express @types/hpp @types/jsonwebtoken @types/morgan @types/nodemailer
+
+# Add .env
+touch .env.example
+touch .env
+
+# Se corre con el siguiente script
+# bash node.sh
